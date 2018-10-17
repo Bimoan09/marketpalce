6d10f62f1702ba982fb3bce0442576cedb06d5d8
bimoan09 <bimoan09@gmail.com>
2018-10-17 10:20:31 +0700

    if products NULL show allert

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -6,7 +6,7 @@
 
 
 
-   <div class="row">
+                    <div class="row">
                         <form action="{{ url()->current() }}">
                             <div class="col-md-10">
                                 <input type="text" name="keyword" class="form-control" placeholder="Cari berdasarkan Nama Produk,ID dan Tanggal..." value="{{ request('keyword') }}">
@@ -20,48 +20,43 @@
                     </div>
                     <br>
 
-<table class="table table-bordered table-responsive table-hover order-column">
-    <thead>
-    <tr>
-    
-        <th>Nama Product</th>
-        <th>Kategori</th>
-        <th>Size</th>
-        <th>Images</th>
-        <th>Aksi</th>
-        
-            </tr>
-        </thead>
-    <tbody>
-@if($product != NULL)
-      @foreach($products as $produk)
-                    <tr>
+        @if(!$products->isEmpty() )
+        <table class="table table-bordered table-responsive table-hover order-column">
+            <thead>
+                <tr>
+                    
+                    <th>Nama Product</th>
+                    <th>Kategori</th>
+                    <th>Size</th>
+                    <th>Images</th>
+                    <th>Aksi</th>
                         
-                        <td>{{ $produk->name }}</td>
-                        <td>{{ $produk->category->name }}</td>
-                        <td>{{ $produk->size }}</td>
-                        <td>{{ $produk->images }}</td>
-                        <td>
-                              <form action="{{ action('ProductsController@destroy', $produk->id) }}" method="POST">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-                                <a href="{{ url('kontak_edit',$produk->id) }}" class=" btn btn-sm btn-primary">Edit</a>
-                                <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('Yakin ingin menghapus data?')">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+                </tr>
+            </thead>
+        <tbody>
+            @foreach($products as $produk)
+                <tr>
+                                        
+                    <td>{{ $produk->name }}</td>
+                    <td>{{ $produk->category->name }}</td>
+                    <td>{{ $produk->size }}</td>
+                    <td>{{ $produk->images }}</td>
+                    <td>
+                        <form action="{{ action('ProductsController@destroy', $produk->id) }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <a href="{{ url('kontak_edit',$produk->id) }}" class=" btn btn-sm btn-primary">Edit</a>
+                            <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('Yakin ingin menghapus data?')">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
         @else
-        <h1>kosong</h1>
-{{-- 
-        @empty
-
-        <h3>No products</h3>
-
-    @endforelse --}}
+        <div class="panel-body">
+            <div class="alert alert-warning text-center">Barang Kosong.</div>
+        </div>
+      @endif
 </ul>
-
-
 @endsection
